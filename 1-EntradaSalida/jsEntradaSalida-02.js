0a2feddc56210d2c00499182a867991b53ce9afe
FrancoFW <francogonzales884@gmail.com>
2023-01-31 08:46:52 -0300

    Ejercicios

---
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -0,0 +1,12 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	let nombre; //Variable para guardar nombre
+
+	nombre = prompt("Ingresa tu nombre"); //asignar un valor a la variable
+
+	alert(nombre); //mostrar el valor asignado en la variable
+}
+
